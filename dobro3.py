857c7b55e7a18d3598f7993bf69f0fe63a1625eb
jhonmaicon13 <jhonmaiconfloresgomez@gmail.com>
2026-06-04 17:42:41 -0300

    Add files via upload

---
+++ dobro3.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite um número: '))
+
+dobro = n1 *2
+print('O dobro deste número é: ' ,dobro)
---
+++ viagem.py
@@ -0,0 +1,5 @@
+distancia = float(input('Digite a distância percorrida: '))
+tempo = float(input('Digite quanto tempo foi gasto: '))
+
+velocidade = distancia / tempo 
+print('Sua velocidade e de: ' ,velocidade)
