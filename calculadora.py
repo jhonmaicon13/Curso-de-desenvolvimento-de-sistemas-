da2ad5ce3dfbe63327abb59bd5be6efe81c58a8f
jhonmaicon13 <jhonmaiconfloresgomez@gmail.com>
2026-08-06 12:03:23 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+n1 = int(input('digite um valor: '))
+n2 = int(input('digite outro valor: '))
+
+soma = n1 + n2
+subtração = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+print('a soma do número ', n1, 'é',n2 ,'e',soma)
+print('a subtração do número ', n1, 'é',n2,'e',subtração)
+print('a multiplição do número ', n1, 'é',n2,'e',multiplicacao)
+print('a divisão do número ', n1, 'é',n2,'e',divisao)
